fcc3e816a59da92332ff5aad99b739355da0b46d
Steven Sherry <steven.sherry@affinityforapps.com>
2021-12-07 12:28:02 -0600

    Going for a hail mary

--- pkgs/lunarvim/default.nix
+++ pkgs/lunarvim/default.nix
@@ -11,44 +11,54 @@ let
     export LUNARVIM_RUNTIME_DIR=${homeDir}/.nix-profile/share/lunarvim
     exec nvim -u ${homeDir}/.nix-profile/share/lunarvim/init.lua "$@"
   '';
-in stdenv.mkDerivation rec {
-  name = "lunarvim-${version}";
+  lunarvim = stdenv.mkDerivation rec {
+    name = "lunarvim-${version}";
 
-  src = fetchFromGitHub {
-    owner = "LunarVim";
-    repo = "LunarVim";
-    rev = "6770808bec1ffcada425ae514747f9380e3d3b8d";
-    sha256 = "fHmvxAz9DqAh7pQ4l1OCQkAGSHDg2kNhDSgtngFEFI8=";
+    src = fetchFromGitHub {
+      owner = "LunarVim";
+      repo = "LunarVim";
+      rev = "6770808bec1ffcada425ae514747f9380e3d3b8d";
+      sha256 = "fHmvxAz9DqAh7pQ4l1OCQkAGSHDg2kNhDSgtngFEFI8=";
+    };
+
+    buildInputs = with pkgs; [
+      git
+    ];
+    
+    propagatedBuildInputs = [
+      pkgs.tree-sitter
+      pkgs.nodePackages.neovim
+      pkgs.python39Packages.pynvim
+      pkgs.ripgrep
+      pkgs.fd
+      lvimBin
+    ];
+
+    buildPhase = ''
+    echo "Do Nothing"
+    '';
+
+    installPhase = ''
+      cp ${lvimBin}/bin/lvim $out/bin
+      mkdir -p ${lunarvimRuntimeDir}/lvim 
+      cp -r . ${lunarvimRuntimeDir}/lvim
+    '';
+
+    meta = with lib; {
+      description = "An IDE Layer for Neovim 0.5.0+";
+      homepage = "https://lunarvim.org";
+      license = licenses.gpl3Only;
+      platforms = platforms.all;
+      maintainers = with maintainers; [ ChristianChiarulli ];
+    };
   };
-
-  buildInputs = with pkgs; [
-    git
-  ];
-  
-  propagatedBuildInputs = [
-    pkgs.tree-sitter
-    pkgs.nodePackages.neovim
-    pkgs.python39Packages.pynvim
-    pkgs.ripgrep
-    pkgs.fd
-    lvimBin
-  ];
-
-  buildPhase = ''
-  echo "Do Nothing"
-  '';
-
-  installPhase = ''
-    cp ${lvimBin}/bin/lvim $out/bin
-    mkdir -p ${lunarvimRuntimeDir}/lvim 
-    cp -r . ${lunarvimRuntimeDir}/lvim
-  '';
-
-  meta = with lib; {
-    description = "An IDE Layer for Neovim 0.5.0+";
-    homepage = "https://lunarvim.org";
-    license = licenses.gpl3Only;
-    platforms = platforms.all;
-    maintainers = with maintainers; [ ChristianChiarulli ];
-  };
+in pkgs.symlinkJoin {
+  name = "lvim";
+  paths = [ pkgs.neovim ];
+  buildInputs = [ pkgs.makeWrapper ];
+  postBuild = ''
+    export LUNARVIM_CONFIG_DIR=${homeDir}/.config/lvim
+    export LUNARVIM_RUNTIME_DIR=${homeDir}/.nix-profile/share/lunarvim
+    wrapProgram $out/bin/nvim -u ${homeDir}/.nix-profile/share/lunarvim/init.lua
+ '';
 }
