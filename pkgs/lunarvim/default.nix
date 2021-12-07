e735f7750b15ee32dd8f81522818bad9d7423f91
Steven Sherry <steven.sherry@affinityforapps.com>
2021-12-07 14:07:10 -0600

    asdffdsa

--- pkgs/lunarvim/default.nix
+++ pkgs/lunarvim/default.nix
@@ -5,55 +5,52 @@
 let 
   version = "unstable";
   lunarvimRuntimeDir = "$out/share/lunarvim";
-  lvimBin = writeScriptBin "lvim" ''
+  lvimBin = writeScriptBin "nlvim" ''
     #!/bin/sh
     export LUNARVIM_CONFIG_DIR=${homeDir}/.config/lvim
     export LUNARVIM_RUNTIME_DIR=${homeDir}/.local/share/lunarvim
     exec ${pkgs.neovim}/bin/nvim -u ${homeDir}/.local/share/lunarvim/lvim/init.lua "$@"
   '';
-  lunarvim = stdenv.mkDerivation rec {
-    name = "lunarvim-${version}";
+  # lunarvim = stdenv.mkDerivation rec {
+  #   name = "lunarvim-${version}";
 
-    src = fetchFromGitHub {
-      owner = "LunarVim";
-      repo = "LunarVim";
-      rev = "6770808bec1ffcada425ae514747f9380e3d3b8d";
-      sha256 = "fHmvxAz9DqAh7pQ4l1OCQkAGSHDg2kNhDSgtngFEFI8=";
-    };
+  #   src = fetchFromGitHub {
+  #     owner = "LunarVim";
+  #     repo = "LunarVim";
+  #     rev = "6770808bec1ffcada425ae514747f9380e3d3b8d";
+  #     sha256 = "fHmvxAz9DqAh7pQ4l1OCQkAGSHDg2kNhDSgtngFEFI8=";
+  #   };
 
-    buildInputs = with pkgs; [
-      git
-    ];
-    
-    propagatedBuildInputs = [
-      pkgs.tree-sitter
-      pkgs.nodePackages.neovim
-      pkgs.python39Packages.pynvim
-      pkgs.ripgrep
-      pkgs.fd
-    ];
+  #   buildInputs = with pkgs; [
+  #     git
+  #   ];
+  #   
+  #   propagatedBuildInputs = [
+  #     pkgs.tree-sitter
+  #     pkgs.nodePackages.neovim
+  #     pkgs.python39Packages.pynvim
+  #     pkgs.ripgrep
+  #     pkgs.fd
+  #   ];
 
-    buildPhase = ''
-    echo "Do Nothing"
-    '';
+  #   buildPhase = ''
+  #   echo "Do Nothing"
+  #   '';
 
-    installPhase = ''
-      mkdir -p ${lunarvimRuntimeDir}/lvim 
-      cp -r . ${lunarvimRuntimeDir}/lvim
-    '';
+  #   installPhase = ''
+  #     mkdir -p ${lunarvimRuntimeDir}/lvim 
+  #     cp -r . ${lunarvimRuntimeDir}/lvim
+  #   '';
 
-    meta = with lib; {
-      description = "An IDE Layer for Neovim 0.5.0+";
-      homepage = "https://lunarvim.org";
-      license = licenses.gpl3Only;
-      platforms = platforms.all;
-      maintainers = with maintainers; [ ChristianChiarulli ];
-    };
-  };
+  #   meta = with lib; {
+  #     description = "An IDE Layer for Neovim 0.5.0+";
+  #     homepage = "https://lunarvim.org";
+  #     license = licenses.gpl3Only;
+  #     platforms = platforms.all;
+  #     maintainers = with maintainers; [ ChristianChiarulli ];
+  #   };
+  # };
 in pkgs.symlinkJoin {
-  name = "nlvim";
-  paths = [ lvimBin pkgs.neovim lunarvim ];
-  propagatedBuildInputs = [
-    lunarvim
-  ];
+  name = "nlvim-link";
+  paths = [ lvimBin pkgs.neovim ];
 }
